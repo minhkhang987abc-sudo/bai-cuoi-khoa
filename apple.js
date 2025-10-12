3cdef51d40bc310b93e905954b6680b63eee0711
minh khang <minhkhang987abc@gmail.com>
2025-10-12 15:58:58 +0700

    them cac trang con

---
+++ apple.js
@@ -0,0 +1,29 @@
+const articlesEl = document.querySelector("#articles");
+
+async function getData() {
+  try {
+    const response = await fetch(
+      `https://newsapi.org/v2/everything?q=apple&from=2025-10-11&to=2025-10-11&sortBy=popularity&apiKey=04eaf6e84ed14b06979b2ec7c5b8f509`
+    );
+
+    const data = await response.json();
+
+    for (let i = 0; i <= data.articles.length; i++) {
+      console.log(data.articles[i]);
+      articlesEl.innerHTML += `<a
+        href="${data.articles[i].url}"
+        class="single-article"
+      >
+        <img src="${data.articles[i].urlToImage}" alt="" />
+        <h3>${data.articles[i].title}</h3>
+        <p>
+          ${data.articles[i].description}
+        </p>
+      </a>`;
+    }
+  } catch (error) {
+    console.error("Lỗi rồi ====>", error);
+  }
+}
+
+getData();
---
+++ buss.js
@@ -0,0 +1,29 @@
+const articlesEl = document.querySelector("#articles");
+
+async function getData() {
+  try {
+    const response = await fetch(
+      `https://newsapi.org/v2/top-headlines?country=us&apiKey=04eaf6e84ed14b06979b2ec7c5b8f509`
+    );
+
+    const data = await response.json();
+
+    for (let i = 0; i <= data.articles.length; i++) {
+      console.log(data.articles[i]);
+      articlesEl.innerHTML += `<a
+        href="${data.articles[i].url}"
+        class="single-article"
+      >
+        <img src="${data.articles[i].urlToImage}" alt="" />
+        <h3>${data.articles[i].title}</h3>
+        <p>
+          ${data.articles[i].description}
+        </p>
+      </a>`;
+    }
+  } catch (error) {
+    console.error("Lỗi rồi ====>", error);
+  }
+}
+
+getData();
